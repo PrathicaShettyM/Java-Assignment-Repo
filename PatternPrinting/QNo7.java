eb0d0b1ccb6763d94c54b5dcd2fb2b8ed6e3b03a
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2024-02-04 18:52:10 +0530

    questions 7 and 8 of pattern printing

---
+++ PatternPrinting/QNo7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class QNo7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. of rows: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the no. of cols: ");
+        int m = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0 ; j < m; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
