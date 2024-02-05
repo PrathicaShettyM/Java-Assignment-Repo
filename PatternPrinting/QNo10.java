b8ec49f93a72ea95d2d705f8bce084678e9fe758
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2024-02-05 07:58:39 +0530

    questions 9 and 10 of pattern printing

---
+++ PatternPrinting/QNo10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class QNo10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. of rows: ");
+        int n = sc.nextInt();
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = i; j > 0; j--){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
